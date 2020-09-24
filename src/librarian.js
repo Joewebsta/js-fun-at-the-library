6218ef0b7a2d92d67c756e7a60103988dde88e0e
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-24 18:45:39 -0400

    Initial commit.

---
+++ src/librarian.js
@@ -0,0 +1,11 @@
+var checkoutBook = require('../src/library.js').checkoutBook;
+var searchShelf = require('../src/shelf.js').searchShelf;
+
+class Librarian {
+  constructor() {
+
+  }
+  
+}
+
+module.exports = Librarian;
