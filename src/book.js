d4af4c4d54e0304158ee3d1c9b94d88f2c5343d2
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-25 16:15:19 -0400

    Write code to pass book.js createTitle, saveReview and saveReview tests.

--- src/book.js
+++ src/book.js
@@ -1,11 +1,25 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviewArr) {
+  if(!reviewArr.includes(review)) {
+    reviewArr.push(review);
+  }
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
   // editBook: editBook
