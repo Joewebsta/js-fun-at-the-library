1119f7946cc587dba3655de5930108f2d8db4837
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-25 17:57:31 -0400

    Pass unshelfBook tests.

--- src/shelf.js
+++ src/shelf.js
@@ -1,12 +1,20 @@
+const book = require("./book");
+
 function shelfBook(book, shelf) {
   if (shelf.length === 3) return
   
   shelf.push(book);
 }
 
+function unshelfBook(title, shelf) {
+
+  bookIdx = shelf.findIndex(book => book.title === title);
+  shelf.splice(bookIdx, 1);      
+}
+
 module.exports = {
   shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
+  unshelfBook: unshelfBook,
   // listTitles: listTitles,
   // searchShelf: searchShelf
 };
