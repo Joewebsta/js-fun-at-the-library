2f97116ba562db03baad2d35e0844e78dd232f8d
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-26 10:10:37 -0400

    Pass Library createLibrary tests.

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
+function createLibrary(name) {
+  return {
+    name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
 
 
 module.exports = {
-  // createLibrary: createLibrary,
+  createLibrary: createLibrary,
   // addBook: addBook,
   // checkoutBook: checkoutBook
 };
